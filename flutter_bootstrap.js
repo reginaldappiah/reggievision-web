bda89d8d3729d95d988ecfa16cc47f55a8be64bf
Reginald Appiah <reginald.appiah7@gmail.com>
2025-05-07 20:47:30 -0400

    🔥 Auto-deploy at Wed May  7 20:47:30 EDT 2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"453dd7174cc6555e3e9eb5f006c86e6ec221a0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3114005756"
+    serviceWorkerVersion: "405032476"
   }
 });
